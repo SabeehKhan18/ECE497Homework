adcc78b23ccec31992a7fb31fbf6ab027de088a8
Sabeeh Khan <sabeeh.khan14@gmail.com>
2016-09-05 22:24:10 +0000

    added comments, code clean

--- HW01/main.c
+++ HW01/main.c
@@ -8,6 +8,7 @@
  */
 
 int main(void) {
+	
 	// The array where the sketch will be held
 	char array[8][8];
 	int x = sizeof(array[0]);
@@ -21,8 +22,10 @@ int main(void) {
 			}
 		}
 	}
-
+	
+	// Print commands to use
 	printf("Use 'mark','up','down','left','right','clear' as inputs to control\n");
+	printf("Use 'exit' to exit program\n");
 
 	// Set the pen at 0,0
 	signed char penX = 0;
@@ -32,13 +35,15 @@ int main(void) {
 	clear();
 
 	while(1) {
+		// Print out pen location
 		printf("Pen is at %d, %d\n",penX,penY);
 		printf("   ");
 		for (int i = 0; i < x; i++) {
 			printf("%d ",i);
 		}
 		printf("\n");
-		
+
+		// Print out the board	
 		for (int i = 0; i < y; i++) {
 			printf("%d: ",i);
 			for (int k = 0; k < x; k++) {	
@@ -55,9 +60,11 @@ int main(void) {
 		else if (input[0] == 'l') penX--;
 		else if (input[0] == 'r') penX++;
 		else if (input[0] == 'c') clear();
+		else if (input[0] == 'e') return 0;
 		else if (input[0] == 'm') array[penY][penX] = 88;
 		else printf("Improper input");
-		
+	
+		// Function to print error message	
 		void printError(void) {
 			printf("Improper move command. Resetting.\n");
 		}
