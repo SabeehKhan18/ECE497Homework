783c733f3da37c41a13276875c442f88c798f1ed
Sabeeh Khan <khansa@rose-hulman.edu>
2016-09-19 04:16:24 +0000

    added interrupt support for 2x tmp101 sensors

--- hw03/i2c_tmp101.sh
+++ hw03/i2c_tmp101.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
-# Config and read file for tmp101s. Collaborated with Hazenhamather
+#Point registers to thermostat
+i2cset -y 2 0x49 0x00 
+i2cset -y 2 0x4a 0x00 
+
+temp1=$(i2cget -y 2 0x49)
+temp2=$(i2cget -y 2 0x4a)
+
+temp1high=$((temp1 + 2))
+temp2high=$((temp2 + 2))
+
+temp1low=$((temp1 - 2))
+temp2low=$((temp1 - 2))
+
+echo 'Initial temps'
+echo 'Sensor 1:'
+echo $temp1
+echo 'Sensor 2:'
+echo $temp2
 
 #Setting up alerts (thermostat mode)[Polarity and thermostat high]
 i2cset -y 2 0x49 0x01 0x06
 i2cset -y 2 0x4a 0x01 0x06
 
 #Setting high and low temps for both pieces
-i2cset -y 2 0x49 0x02 0x20 #Setting Low on first
-i2cset -y 2 0x49 0x03 0x18 #Setting High on first
+i2cset -y 2 0x49 0x02 $temp1low #Setting Low on first
+i2cset -y 2 0x49 0x03 $temp1high #Setting High on first
 
-i2cset -y 2 0x4a 0x02 0x20 #Setting Low on second
-i2cset -y 2 0x4a 0x03 0x18 #Setting High on second
+i2cset -y 2 0x4a 0x02 $temp2low #Setting Low on second
+i2cset -y 2 0x4a 0x03 $temp2high #Setting High on second
 
 #Pointing registers back to thermostat
-i2cset -y 2 0x49 0x00 #Set the pointer register back to temperature register
-i2cset -y 2 0x4a 0x00 #Set the pointer register back to temperature register
+i2cset -y 2 0x49 0x00 
+i2cset -y 2 0x4a 0x00 
 
-temp1=$(i2cget -y 2 0x49)
-temp2=$(i2cget -y 2 0x4a)
+echo 'Executing a script'
+./tmp_alert.js
 
-temp1=$((temp1*9/5 + 32))
-temp2=$((temp2*9/5 + 32))
-
-echo $temp1
-echo $temp2
+echo 'Sensor 1'
+i2cget -y 2 0x49
+echo 'Sensor 2'
+i2cget -y 2 0x4a
